2af575763d91f07f59e8d5b1ae85f6d36fce15cd
Rajat Pandey <rajatpandey7800@gmail.com>
2024-06-04 23:32:48 +0530

    June-4 2024

---
+++ src/rajat/array/FourSum.java
@@ -0,0 +1,9 @@
+package rajat.array;
+
+public class FourSum {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/rajat/array/MoveAllZeros.java
@@ -0,0 +1,28 @@
+package rajat.array;
+
+public class MoveAllZeros {
+    public static void swap(int arr[], int a, int b){
+        int c = arr[a];
+        arr[a] = arr[b];
+        arr[b] = c;
+    }
+    public static void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int j = 0;
+
+       for(int i = 0; i < n; i++){
+           if(nums[i] != 0){
+               swap(nums,i, j);
+               j++;
+           }
+       }
+    }
+    public static void main(String[] args) {
+        int arr[] = {0,1,0,3,12};
+        int n = arr.length;
+        moveZeroes(arr);
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
